cd36357e30717e6d715c6c332b64f8cd811c998b
deAd-otAku327 <almchech@mail.ru>
2025-05-05 15:34:39 +0300

    improved dto responses

--- internal/types/dto/responses.go
+++ internal/types/dto/responses.go
@@ -1,61 +1,60 @@
 package dto
 
 import (
-	"task-tracker-service/internal/types/enum"
 	"time"
 )
 
-type User struct {
+type UserResponse struct {
 	ID       int32  `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 }
 
-type GetUsersResponse []*User
+type GetUsersResponse []*UserResponse
 
-type Task struct {
-	ID          int32           `json:"id"`
-	Title       string          `json:"title"`
-	Status      enum.TaskStatus `json:"status"`
-	Discription *string         `json:"discription,omitempty"`
-	Assignie    *bool           `json:"assignie,omitempty"`
-	Board       *bool           `json:"board,omitempty"`
-	UpdatedAt   time.Time       `json:"updated_at"`
+type TaskResponse struct {
+	ID          int32     `json:"id"`
+	Title       string    `json:"title"`
+	Discription *string   `json:"discription,omitempty"`
+	Status      string    `json:"status"`
+	Assignie    *bool     `json:"assignie,omitempty"`
+	Board       *bool     `json:"board,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type GetTasksResponse []*Task
+type GetTasksResponse []*TaskResponse
 
 type GetTaskByIDResponse struct {
-	Task        *Task      `json:"task"`
-	Comments    []*Comment `json:"comments,omitempty"`
-	Author      *User      `json:"author"`
-	Assignie    *User      `json:"assignie,omitempty"`
+	Task        *TaskResponse      `json:"task"`
+	Comments    []*CommentResponse `json:"comments,omitempty"`
+	Author      *UserResponse      `json:"author"`
+	Assignie    *UserResponse      `json:"assignie,omitempty"`
 	LinkedBoard struct {
 		ID   string `json:"id,omitempty"`
 		Name string `json:"name,omitempty"`
 	} `json:"linkedBoard"`
 }
 
-type Comment struct {
+type CommentResponse struct {
 	ID         string    `json:"id,omitempty"`
 	AuthorName string    `json:"author"`
 	Text       string    `json:"text"`
 	DateTime   time.Time `json:"dateTime"`
 }
 
-type Dashboard struct {
+type DashboardResponse struct {
 	ID          string    `json:"id"`
 	Title       string    `json:"title"`
 	Discription string    `json:"discription"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type GetDashboardsResponse []*Dashboard
+type GetDashboardsResponse []*DashboardResponse
 
 type GetDashboardByIDResponse struct {
-	Dashboard *Dashboard `json:"dashboard"`
-	Tasks     []*Task    `json:"tasks"`
-	Admins    []*User    `json:"admin"`
+	Dashboard *DashboardResponse `json:"dashboard"`
+	Tasks     []*TaskResponse    `json:"tasks"`
+	Admins    []*UserResponse    `json:"admin"`
 }
 
 type Error struct {
