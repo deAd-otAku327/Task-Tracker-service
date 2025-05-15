ebe79518b6b4a57987e6ff7793d27484811a9102
deAd-otAku327 <almchech@mail.ru>
2025-05-15 03:07:58 +0300

    fixed migrations: added named constraints

--- internal/storage/migrations/4_create_tasks_table.up.sql
+++ internal/storage/migrations/4_create_tasks_table.up.sql
@@ -11,8 +11,17 @@ CREATE TABLE IF NOT EXISTS tasks
     title TEXT NOT NULL,
     description TEXT,
     status task_status_enum NOT NULL DEFAULT 'in_progress',
-    author_id INTEGER REFERENCES users(id),
-    assignie_id INTEGER REFERENCES users(id),
-    board_id INTEGER REFERENCES dashboards(id) ON DELETE SET NULL,
+    author_id INTEGER,
+    assignie_id INTEGER,
+    board_id INTEGER,
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+ALTER TABLE tasks ADD CONSTRAINT tasks_author_id_fk
+FOREIGN KEY (author_id) REFERENCES users(id);
+
+ALTER TABLE tasks ADD CONSTRAINT tasks_assignie_id_fk
+FOREIGN KEY (assignie_id) REFERENCES users(id);
+
+ALTER TABLE tasks ADD CONSTRAINT tasks_board_id_fk
+FOREIGN KEY (board_id) REFERENCES dashboards(id) ON DELETE SET NULL;
--- internal/storage/migrations/5_create_comments_table.up.sql
+++ internal/storage/migrations/5_create_comments_table.up.sql
@@ -1,8 +1,14 @@
 CREATE TABLE IF NOT EXISTS comments
 (
     id SERIAL PRIMARY KEY,
-    task_id INTEGER NOT NULL REFERENCES tasks(id),
-    author_id INTEGER NOT NULL REFERENCES users(id),
+    task_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
     text TEXT NOT NULL,
     dateTime TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+ALTER TABLE comments ADD CONSTRAINT comments_task_id_fk
+FOREIGN KEY (task_id) REFERENCES tasks(id);
+
+ALTER TABLE comments ADD CONSTRAINT comments_author_id_fk
+FOREIGN KEY (author_id) REFERENCES users(id);
